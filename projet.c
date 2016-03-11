dfe1972a17089d45fb27100aed911687db420abe
Arnaud Vota <15009003@e003m04.istic.univ-rennes1.fr>
2016-03-11 11:15:51 +0100

    ajout initMatriceDebits

--- projet.c
+++ projet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define NB_USERS
-#define BUFFERS_PER_USER
+#define NB_USERS 10
+#define BUFFERS_PER_USER 10
 
 typedef struct Packet Packet;
 struct Packet
@@ -14,25 +14,47 @@ typedef struct Buffer Buffer;
 struct Buffer
 {
 	Packet thePacket;
-	Buffer* nextBuffer = NULL;
+	Buffer* nextBuffer;
 };
 
 typedef struct User User;
 struct User
 {
 	int debitActuel, debitMoyen;
+	int debitsActuels[128][5];
 	Buffer sendBuffer[BUFFERS_PER_USER];
 };
+
 typedef struct Antenne Antenne;
 struct Antenne
 {
-	
+	User usersNear[NB_USERS/2];
+	User usersFar[NB_USERS/2];
 };
 
 int trameToSend[128][5];
 
+int initMatriceDebits(Antenne *antenne);
 int main(){
-	Antenne monAntenne;
-		
+	Antenne* monAntenne;
 	
+	//---INITIALISATIONS---
+	if(initAntenne(monAntenne) == -1){
+		printf("Erreur lors de l'initialisation de l'antenne\n");
+	}
+	else{printf("Initialisation Antenne: OK\n");}
+
+
+	//---BOUCLE PRINCIPALE---
+	//Initilisations des matrices de débit des utilisateurs  
+
+
+}
+
+int initMatriceDebits(Antenne *antenne){
+	int i;
+
+	for(i = 0; i < NB_USERS/2; i++){
+		
+	}
 }
