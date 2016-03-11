ff32f5b106e4ebf29ac97f867913dcb421250324
Nicolas LeGall <13002442@e003m02.istic.univ-rennes1.fr>
2016-03-11 10:25:55 +0100

    les buffers

---
+++ projet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define NB_USERS
+
+typedef struct Packet Packet;
+struct Packet
+{
+	int dateCreation;
+};
+
+typedef struct Buffer Buffer;
+struct Buffer
+{
+	Packet thePacket;
+	Buffer* nextBuffer = NULL;
+};
+
+typedef struct Antenne Antenne;
+struct Antenne
+{
+	//Creation d'un tableau contenant le premier buffer de chaque utilisateur
+	Buffer sendBuffer[NB_USERS];
+};
+
+int trameToSend[128][5];
+
+int main(){
+	Antenne monAntenne;
+		
+	
+}
