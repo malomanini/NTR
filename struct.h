ef77be84de4e999cc60cb0cf51227747fa5fb3ef
Arnaud Vota <15009003@e105m09.istic.univ-rennes1.fr>
2016-03-15 11:54:26 +0100

    ajout de struct

---
+++ struct.h
@@ -0,0 +1,34 @@
+#define NB_USERS 10
+#define BUFFERS_PER_USER 10
+
+typedef struct Packet Packet;
+struct Packet
+{
+	int dateCreation;
+	int bitsRestants;
+};
+
+typedef struct Buffer Buffer;
+struct Buffer
+{
+	Packet thePacket;
+	Buffer* nextBuffer;
+};
+
+typedef struct User User;
+struct User
+{
+
+	int bufferVide;
+	int debitMoyen;
+	int debitsActuels[128];
+	Buffer firstBuffer;
+	Buffer lastBuffer;
+};
+
+typedef struct Antenne Antenne;
+struct Antenne
+{
+	User usersNear[NB_USERS/2];
+	User usersFar[NB_USERS/2];
+};
